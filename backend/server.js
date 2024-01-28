d1b9d7277d02e51a257d2fad76644c5129347acd
Suchethan2002 <102217992+Suchethan2002@users.noreply.github.com>
2024-01-28 09:00:00 +0530

    up

--- backend/server.js
+++
@@ -1,9 +0,0 @@
-const mongoose=require("mongoose")
-const mongouri="mongodb+srv://20211a05c2:vishu123@cluster0.kb860pu.mongodb.net/"
-mongoose.connect(mongouri)
-.then(()=>{
-    console.log("mongodb connected");
-})
-.catch(()=>{
-    console.log("failed");
-})
